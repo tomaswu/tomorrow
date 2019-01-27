113480b34d6070fecc6f39e40aed9f41e6c6da25
Tomas <tomasiwt@gmail.com>
2019-01-27 21:03:22 +0800

    selenium test

---
+++ test_selenium.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+'''
+    @Author: Tomas Wu 
+    @Date: 2019-01-27 20:20:25 
+    @Desc: 
+'''
+from selenium import webdriver
+
+broswer=webdriver.Firefox()
+
+broswer.get('http://www.github.com')
